2e26c3458abd84aaaa0c12056a5f7e539dd0a365
danielgn95 <daniel.gn@globant.com>
2022-03-18 11:07:52 -0500

    wip1

--- src/main/java/com/challenges/challenge8/repositories/BookRepository.java
+++ src/main/java/com/challenges/challenge8/repositories/BookRepository.java
@@ -1,4 +1,9 @@
 package com.challenges.challenge8.repositories;
 
-public class BookRepository {
+import com.challenges.challenge8.entities.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository <Book, Long> {
 }
--- src/main/java/com/challenges/challenge8/repositories/OrderRepository.java
+++ src/main/java/com/challenges/challenge8/repositories/OrderRepository.java
@@ -1,4 +1,9 @@
 package com.challenges.challenge8.repositories;
 
-public class OrderRepository {
+import com.challenges.challenge8.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository <Order, Long> {
 }
