c0884949650907c6492b4b0adc9c902ee04165cf
danielgn95 <daniel.gn@globant.com>
2022-03-18 09:55:24 -0500

    Initial commit

---
+++ src/main/java/com/challenges/challenge8/Challenge8Application.java
@@ -0,0 +1,13 @@
+package com.challenges.challenge8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Challenge8Application.class, args);
+    }
+
+}
