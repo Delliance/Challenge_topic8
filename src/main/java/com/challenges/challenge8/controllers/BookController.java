4f581f8ae5f25d364c8fcaeedf7942ec49cd7643
danielgn95 <daniel.gn@globant.com>
2022-03-20 09:24:49 -0500

    wip3

--- src/main/java/com/challenges/challenge8/controllers/BookController.java
+++ src/main/java/com/challenges/challenge8/controllers/BookController.java
@@ -1,4 +1,40 @@
 package com.challenges.challenge8.controllers;
 
+import com.challenges.challenge8.entities.Book;
+import com.challenges.challenge8.entities.BookStatus;
+import com.challenges.challenge8.services.BookService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("bookstore/v1/book")
+@AllArgsConstructor
 public class BookController {
+
+    private BookService bookService;
+
+    @PostMapping
+    public void addNewBook(@RequestBody Book book){
+        bookService.addNewBook(book);
+    }
+
+    @PutMapping
+    public void editBook(@RequestBody Book book){
+        bookService.updateBook(book);
+    }
+
+    @GetMapping(path = "/findByStatus")
+    public List<Book> getBooksByStatus (@RequestBody String status){
+        return bookService.getBooksByStatus(BookStatus.valueOf(status));
+    }
+
+    @GetMapping(path = "/{id}")
+    public List<Book> getBooksById(@PathVariable("id") long id){
+        return bookService.getBooksById(id);
+    }
+
+
+
 }
--- src/main/java/com/challenges/challenge8/controllers/OrderController.java
+++ src/main/java/com/challenges/challenge8/controllers/OrderController.java
@@ -1,4 +1,28 @@
 package com.challenges.challenge8.controllers;
 
+import com.challenges.challenge8.entities.Order;
+import com.challenges.challenge8.services.OrderService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("bookstore/v1/store")
+@AllArgsConstructor
 public class OrderController {
+
+    private OrderService orderService;
+
+    @GetMapping(path = "/inventory")
+    public void getOrdersByStatus (){
+        orderService.getQuantityByStatus();
+    }
+
+    @PostMapping(path = "/order")
+    public Order postOrderAndReturnIt (@RequestBody Order order){
+        return orderService.placeOrderAndReturnAcceptedOrder(order);
+    }
+
+
 }
